64aeb4f4e740c3cef501edb1660537cca3015ba7
Artsiom Yermak <Artsiom.Yermak@itechart-group.com>
2022-04-14 16:12:43 +0300

    dev_29

---
+++ LibBot/LibBot/Controllers/WebhookController.cs
@@ -0,0 +1,17 @@
+﻿using LibBot.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace LibBot.Controllers;
+
+public class WebhookController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post([FromServices] HandleUpdateService handleUpdateService,
+                                          [FromBody] Update update)
+    {
+        await handleUpdateService.EchoAsync(update);
+        return Ok();
+    }
+}
---
+++ LibBot/LibBot/Services/HandleUpdateService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace LibBot.Services;
+
+public class HandleUpdateService
+{
+    private readonly ITelegramBotClient _botClient;
+
+    public HandleUpdateService(ITelegramBotClient botClient)
+    {
+        _botClient = botClient;
+    }
+
+    public async Task EchoAsync(Update update)
+    {
+       await _botClient.SendTextMessageAsync(update.Message.Chat.Id, update.Message.Text);
+    }
+}
