0277d87c922f0b1a4be882652b172f59686f8f08
Artyom Yermak <55196488+Artierm@users.noreply.github.com>
2022-05-06 14:47:54 +0300

    Update Program.cs

--- LibBot/Program.cs
+++ LibBot/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Net;
 
 namespace LibBot;
 
@@ -12,13 +10,10 @@ public class Program
         CreateHostBuilder(args).Build().Run();
     }
   
-     public static IHostBuilder CreateHostBuilder(string[] args) =>
-                Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.ConfigureKestrel(serverOptions =>
-                        {
-                            serverOptions.Listen(IPAddress.Any, Convert.ToInt32(Environment.GetEnvironmentVariable("PORT")));
-                        }).UseStartup<Startup>();
-                    });
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
