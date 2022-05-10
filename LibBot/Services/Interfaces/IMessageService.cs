5a4f03971462129f8b90776aab64314c464e7001
Anton Dombrovskiy <anton.dombrovskij.cz@gmail.com>
2022-05-10 14:34:27 +0300

    Change main menu

--- LibBot/Services/Interfaces/IMessageService.cs
+++ LibBot/Services/Interfaces/IMessageService.cs
@@ -12,7 +12,7 @@ public interface IMessageService
     Task<Message> SendTextMessageAndClearKeyboardAsync(ITelegramBotClient bot, long chatId, string message);
     Task<Message> AskToEnterOutlookLoginAsync(ITelegramBotClient bot, Message message);
     Task<Message> AskToEnterAuthCodeAsync(ITelegramBotClient bot, Message message);
-    Task<Message> SayDefaultMessageAsync(ITelegramBotClient bot, Message message);
+    Task<Message> SendWelcomeMessageAsync(long chatId);
     Task<Message> AksToEnterSearchQueryAsync(ITelegramBotClient bot, Message message);
     Task<Message> SayThisBookIsAlreadyBorrowAsync(ITelegramBotClient bot, Message message);
     Task CreateYesAndNoButtons(CallbackQuery callbackQuery, string message);
@@ -23,4 +23,6 @@ public interface IMessageService
     Task EditMessageAfterYesAndNoButtons(ITelegramBotClient bot, CallbackQuery callbackQuery, string messageText);
     Task DisplayInlineButtonsWithMessage(Message message, string messageText, params string[] buttons);
     Task UpdateInlineButtonsWithMessage(long chatId, int messageId, string messageText, string[] bookPaths);
+    Task SendLibraryMenuMessageAsync(long chatId);
+    Task SendMessageWithInlineKeyboardAsync(long chatId, string message, string[] keys);
 }
