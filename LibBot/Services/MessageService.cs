5dbff044d6f9cf68d2fb2d361749a49e236d353a
Anton Dombrovskiy <anton.dombrovskij.cz@gmail.com>
2022-05-11 13:47:49 +0300

    a

--- LibBot/Services/MessageService.cs
+++ LibBot/Services/MessageService.cs
@@ -31,7 +31,7 @@ namespace LibBot.Services
         }
         public async Task<Message> SayHelloFromArtyom(ITelegramBotClient bot, Message message)
         {
-            return await _botClient.SendTextMessageAsync(message.Chat.Id, "New message", replyMarkup: replyKeyboardMarkup);
+            return await _botClient.SendTextMessageAsync(message.Chat.Id, "New message a ", replyMarkup: replyKeyboardMarkup);
         }
         public async Task<Message> SayDefaultMessage(ITelegramBotClient bot, Message message)
         {
