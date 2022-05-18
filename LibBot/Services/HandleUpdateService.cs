1d1b4fc5aeb588f8df9b56ef8bccd13f6d57ecc1
Artsiom Yermak <Artsiom.Yermak@itechart-group.com>
2022-05-18 16:15:20 +0300

    dev_107

--- LibBot/Services/HandleUpdateService.cs
+++ LibBot/Services/HandleUpdateService.cs
@@ -286,8 +286,16 @@ public class HandleUpdateService : IHandleUpdateService
         }
 
         var allBooks = await GetBookDataResponses(data.PageNumber, data);
-        await _messageService.DisplayBookButtons(data.ChatId, _resourceReader.GetString("EmptyLibrary") + $"{Environment.NewLine}"
-            + GetFiltersAsAStringMessage(data.Filters), allBooks, data.ChatState);
+        if (allBooks.Count != 0)
+        {
+            await _messageService.DisplayBookButtons(data.ChatId, _resourceReader.GetString("BooksLibrary") + $"{Environment.NewLine}"
+                   + GetFiltersAsAStringMessage(data.Filters), allBooks, data.ChatState);
+        }
+        else
+        {
+            await _messageService.DisplayBookButtons(data.ChatId, _resourceReader.GetString("EmptyLibrary") + $"{Environment.NewLine}"
+          + GetFiltersAsAStringMessage(data.Filters), allBooks, data.ChatState);
+        }
         var chatInfoAllBooks = new ChatDbModel(message.Chat.Id, new List<int>() { message.MessageId + 1 }, ChatState.AllBooks)
         {
             Filters = data.Filters
